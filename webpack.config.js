e28fd76df78bc2452212d76559ece9be0675bfd8
aleksandr gulchenko <ogulchenko@contuctor.com>
2021-02-25 15:20:06 +0200

    asd

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,6 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 
 
 module.exports = federations({
-  name: 'condFormElements',
-  remotes: {
-    condRootMf: 'condRootMf@http://localhost:3001/remoteEntry.js',
-    condAuthPages: 'condAuthPages@http://localhost:3002/remoteEntry.js',
-    condLayouts: 'condLayouts@http://localhost:3003/remoteEntry.js',
-  },
   exposes: {
     './TextInput': './src/app/components/textInput',
   },
